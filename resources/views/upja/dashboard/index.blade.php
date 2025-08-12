7f76ad06922b4a6ae579c25cb14b0de0abc6613d
aditya wastu <adityawastu37@gmail.com>
2025-08-12 09:33:47 +0700

    3 role

--- resources/views/upja/dashboard/index.blade.php
+++ resources/views/upja/dashboard/index.blade.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layout>
+   <div class="p-6">
+      <h1 class="text-2xl font-bold text-green-600 mb-6">UPja</h1>
 
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>Document</title>
-</head>
 
-<body>
-   halaman upja
-</body>
-
-</html>
+</x-layout>
